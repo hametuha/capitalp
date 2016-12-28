28647af8760d951d9d4fe1a8fa97df828859fd17
fumikito <guy@hametuha.com>
2016-12-28 14:00:43 +0900

    Start tracking podcast

--- hooks/assets.php
+++ hooks/assets.php
@@ -8,13 +8,34 @@
  * Register scripts
  */
 add_action( 'init', function() {
+
+	$version = wp_get_theme()->get( 'Version' );
+
 	// Register main theme style
 	wp_register_style( 'twentyseventeen-style', get_template_directory_uri().'/style.css', [], wp_get_theme('twentyseventeen')->get('Version') );
 	// Register this style
-	wp_register_style( 'capitalp', get_stylesheet_directory_uri().'/assets/css/style.css', ['twentyseventeen-style'], wp_get_theme()->get( 'Version' ) );
+	wp_register_style( 'capitalp', get_stylesheet_directory_uri().'/assets/css/style.css', ['twentyseventeen-style'], $version );
+
+	// Register JS
+	wp_register_script( 'capitalp-tracker', get_stylesheet_directory_uri() .'/assets/js/tracker.js', ['jquery'], $version, true );
 } );
 
-
+/**
+ * Register global assets
+ */
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'capitalp' );
 } );
+
+/**
+ * Executed on playlist
+ */
+add_filter( 'ssp_media_player', function( $player, $src, $episode_id ) {
+	wp_enqueue_script( 'capitalp-tracker' );
+	$player .= sprintf(
+		'<span class="capitalp-media-tracker" style="display: none;" data-src="%s" data-episode-id="%s"></span>',
+		esc_attr( $src ),
+		esc_attr( $episode_id )
+	);
+	return $player;
+}, 10, 3 );
---
+++ src/js/tracker.js
@@ -0,0 +1,98 @@
+/**
+ * Description
+ */
+
+/*global ga:false */
+
+jQuery(document).ready(function ($) {
+
+  "use strict";
+
+  function makeUrl(url){
+    return url.replace(/^https?:\/\/[^\/]+/, '');
+  }
+
+  // Track pod cast events
+  $('.podcast_player').each(function(index, player){
+    // Check if tracker exists.
+    var $player = $(this);
+    var $tracker = $player.find('.capitalp-media-tracker');
+    if ( !$tracker.length ) {
+      return true;
+    }
+    // Add event listeners
+    var played = false;
+    var playCount = 0;
+    var episodeId = $tracker.attr('data-episode-id');
+    var href = makeUrl( $tracker.attr('data-src') );
+    // Save play count
+    $player.find('audio,video').bind('play', function(e){
+      // First play
+      if (!played) {
+        played = true;
+        try{
+          ga('send', {
+            hitType: 'pageview',
+            page: href.split('?')[0],
+            location: href
+          });
+        }catch (err){}
+      }
+      try {
+        var data = {
+          hitType      : 'event',
+          eventCategory: $(this).prop('tagName'),
+          eventAction  : 'play',
+          eventLabel   : episodeId,
+          eventValue   : 1
+        };
+        ga('send', data);
+      } catch (err) {}
+    });
+    // Save other event
+    $.each(['ended', 'pause', 'error', 'abort'], function(i, eventType){
+      $player.find('audio,video').bind(eventType, function(event){
+        try {
+          var data = {
+            hitType      : 'event',
+            eventCategory: $(this).prop('tagName'),
+            eventAction  : eventType,
+            eventLabel   : episodeId,
+            eventValue   : 1
+          };
+          ga('send', data);
+        } catch (err) {}
+      });
+    });
+  });
+
+  // Track download event
+  $('.podcast_meta').on( 'click', 'a', function(e){
+    alert('click');
+    var title = $(this).attr('title');
+    var blank = '_blank' == $(this).attr('target');
+    var href  = $(this).attr('href');
+    try{
+      var path = makeUrl(href);
+      var event = {
+        hitType: 'pageview',
+        page: path.split('?')[0],
+        location: path,
+        hitCallback: function(){
+          if (blank) {
+            window.location.href = href;
+          }
+        }
+      };
+      ga('send', event);
+      if ( blank ) {
+        e.preventDefault();
+      }
+    }catch(err){
+      if ( window.console ) {
+        console.log(err)
+      }
+    }
+  });
+
+});
