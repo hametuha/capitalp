a16e894059c225ea11d791230888c81303d912b8
fumikito <guy@hametuha.com>
2017-01-04 14:12:26 +0900

    Change jetpack publicize default text

--- hooks/override.php
+++ hooks/override.php
@@ -5,7 +5,13 @@
  * @package capitalp
  */
 
-// Remove excerpt link
+// Remove excerpt link.
 add_action( 'after_setup_theme', function(){
 	remove_filter( 'excerpt_more', 'twentyseventeen_excerpt_more' );
 } );
+
+// Customize publicize.
+add_filter( 'wpas_default_suffix', function( $suffix ){
+	$suffix .= ' #capitalp ';
+	return $suffix;
+}, 10, 4 );
