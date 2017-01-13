20c8daf3ce9334f6ca40bfd3d499bbbb2b1b603e
fumikito <guy@hametuha.com>
2017-01-13 16:27:04 +0900

    Add mautic URL to admin bar

--- hooks/override.php
+++ hooks/override.php
@@ -15,3 +15,13 @@ add_filter( 'wpas_default_suffix', function( $suffix ){
 	$suffix .= ' #capitalp ';
 	return $suffix;
 }, 10, 4 );
+
+// Add mautic bar
+add_action( 'admin_bar_menu', function( WP_Admin_Bar &$wp_admin_bar ) {
+	$wp_admin_bar->add_menu( array(
+		'parent' => 'site-name',
+		'id'     => 'mautic',
+		'title'  => 'Mautic',
+		'href'   => 'https://capitalp.mautic.net/',
+	) );
+}, 11 );
