4e07b11962d6f9d0b5abc86fe47fedc718183a4b
fumikito <guy@hametuha.com>
2017-11-27 12:03:06 +0900

    Add user list shortcodes.

--- hooks/membership.php
+++ hooks/membership.php
@@ -32,7 +32,9 @@ HTML;
  */
 add_filter( 'ofuse_should_load_style', '__return_false' );
 
-
+/**
+ * Add separator for member only contents
+ */
 add_filter( 'the_content', function( $content ) {
 	if ( ! chiramise_should_check() ) {
 		return $content;
@@ -43,7 +45,56 @@ add_filter( 'the_content', function( $content ) {
 	// This content is members only.
 	ob_start();
 	get_template_part( 'template-parts/module/separator', 'more' );
-	$block = implode( "\n", array_map( 'trim', explode( "\n", ob_get_contents() ) ) );
+	$block = implode( "\n", array_filter( array_map( 'trim', explode( "\n", ob_get_contents() ) ) ) );
 	ob_end_clean();
 	return str_replace( '<!--more-->', $block, get_post()->post_content );
 }, 2 );
+
+/**
+ * Display member list
+ */
+add_shortcode( 'capitalists', function( $args = [], $contents = '' ) {
+	$members = ofuse_members();
+	if ( ! $members ) {
+		return '';
+	}
+	ob_start();
+	foreach ( $members as $member ) {
+		include get_stylesheet_directory() . '/template-parts/block/loop-member.php';
+	}
+	$block = sprintf(
+		"<p class=\"capitalists-desc\">Capital Pは%d人のCapitalistsによって支えられています。</p>\n<div class=\"capitalists-wrapper\">\n%s\n</div>",
+		count( $members ),
+		implode( "\n", array_filter( array_map( 'trim', explode( "\n", ob_get_contents() ) ) ) )
+	);
+	ob_end_clean();
+	return $block;
+} );
+
+/**
+ * プロフィールページ
+ */
+add_action( 'ofuse_membership_table', function( $user ) {
+	?>
+	<tr>
+		<th>
+			<label for="hide_from_capitalist">プライバシー</label>
+		</th>
+		<td>
+			<label>
+				<input type="checkbox" id="hide_from_capitalist" name="hide_from_capitalist" value="1" <?php checked( ! capitalp_is_public_capitalist( $user->ID ) ) ?>/>
+				支援者リストから隠す
+			</label>
+		</td>
+	</tr>
+	<?php
+}, 11 );
+
+// Update license
+add_action( 'ofuse_member_profile_update', function( $user_id, $old_data ) {
+	if ( isset( $_POST['hide_from_capitalist'] ) && $_POST['hide_from_capitalist'] ) {
+		update_user_meta( $user_id, 'hidden_capitalist', 1 );
+	} else {
+		delete_user_meta( $user_id, 'hidden_capitalist' );
+	}
+}, 10, 2 );
---
+++ template-parts/block/loop-member.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Member bempalte
+ * @var WP_User $member
+ */
+?>
+<div class="capitalists-item">
+	
+	<?php if ( capitalp_is_public_capitalist( $member->ID ) ) : ?>
+		<?= get_avatar( $member->ID ) ?>
+		<?php if ( preg_match( '#^https?://.+#u', $member->user_url ) ) : ?>
+			<a href="<?= esc_url( $member->user_url ) ?>" class="capitlist-name">
+				<?= esc_html( $member->display_name ) ?>
+			</a>
+		<?php else: ?>
+			<span class="capitalist-name">
+				<?= esc_html( $member->display_name ) ?>
+			</span>
+		<?php endif; ?>
+	<?php else : ?>
+		<?= get_avatar( 0 ) ?>
+		<span class="capitalist-name">
+			#<?= number_format( $member->ID ) ?>
+		</span>
+	<?php endif; ?>
+</div>
