1fb7df782106c077c78ea55eb5107fd4b6fa481f
fumikito <guy@hametuha.com>
2018-05-31 14:11:13 +0900

    メンバーによる宣伝が弱いので鼓舞するメッセージを追加

--- hooks/notifications.php
+++ hooks/notifications.php
@@ -12,7 +12,7 @@ add_action( 'transition_post_status', function ( $new_status, $old_status, $post
 	}
 	if ( ( 'publish' === $new_status ) && ( 'publish' !== $old_status ) ) {
 		// Post is newly published.
-		$content = '@channel 新しく記事が公開されたワン！ %s';
+		$content = '@channel 新しく記事が公開されたワン！ いっぱい宣伝して欲しいワン！ %s';
 		do_action( 'hameslack', sprintf( $content, get_permalink( $post ) ) );
 	}
 }, 10, 3 );
