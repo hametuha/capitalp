19849952ca70f4239b73d40f6560838a127256a9
fumikito <guy@hametuha.com>
2018-07-03 02:42:07 +0900

    Restore jquery footer moover

---
+++ hooks/assets-jquery.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * jQueryをフッターに動かす
+ */
+add_action( 'init', function() {
+	// 管理画面ではjQueryを削除できない。
+	if ( is_admin() || 'wp-login.php' === basename( $_SERVER['SCRIPT_FILENAME'] ) ) {
+		return;
+	}
+	// 現在のバージョンとURIを保存。
+	// CDNを使いたい方は$jquery_srcのURIを変更してもよい。
+	global $wp_scripts;
+	$jquery = $wp_scripts->registered['jquery-core'];
+	$jquery_ver = $jquery->ver;
+	$jquery_src = $jquery->src;
+	// いったん削除
+	wp_deregister_script( 'jquery' );
+	wp_deregister_script( 'jquery-core' );
+	// 登録しなおし
+	wp_register_script( 'jquery', false, [ 'jquery-core' ], $jquery_ver, true );
+	wp_register_script( 'jquery-core', $jquery_src, [], $jquery_ver, true );
+} );
+
+/**
+ * Move Jetpack share scripts to footer.
+ */
+add_action( 'wp_footer', function() {
+	if ( has_action( 'wp_footer', 'sharing_add_footer' ) ) {
+		remove_action( 'wp_footer', 'sharing_add_footer' );
+		add_action( 'wp_footer', 'sharing_add_footer', 20 );
+	}
+}, 1 );
--- hooks/assets.php
+++ hooks/assets.php
@@ -4,6 +4,7 @@
  */
 
 
+
 /**
  * Register scripts
  */
