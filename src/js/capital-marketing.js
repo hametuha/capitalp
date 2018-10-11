ebe960b6f003a39fb80e567b932ebcb13e877584
fumikito <guy@hametuha.com>
2018-10-11 13:06:46 +0900

    Fix link tracker

--- src/js/capital-marketing.js
+++ src/js/capital-marketing.js
@@ -3,38 +3,33 @@ jQuery(document).ready(function ($) {
   'use strict';
 
   $(document).find('a[href]').each(function(index, link){
-    var href = $(this).attr('href');
+    var href = $(link).attr('href');
     var regexp = /^https?:\/\/(akismet\.com|wordbench\.org|capitalp|hametuha|amazon|(.*\.)?wordpress\.org|itunes\.apple\.com|twitter\.com|www\.facebook.com|www\.instagram\.com|github\.com|wpdocs\.osdn\.jp|b\.hatena\.ne\.jp)/;
-    if ( ! href.test(regexp) && href.test(/^https?:\/\//)) {
+    if ( ! href.test(regexp) && href.test(/^https?:\/\//) && ! $(link).parents('.js-wp-oembed-blog-card').length ) {
       var parts = href.split( '?' );
-      var args = [];
+      var args = {
+        utm_source:   'capitalp',
+        utm_campaign: 'SponsorUs',
+        utm_medium:   'voluntary_link'
+      };
       if ( 1 < parts.length ) {
         $.each(parts[1].split('&'), function(i, param){
           var vars = param.split('=');
-          switch(param[0]){
-            case 'utm_source':
-            case 'utm_campaign':
-            case 'utm_medium':
-              // Do nothing.
-              break;
-            default:
-              args.push(param);
-              break;
-          }
+          args[vars[0]] = vars[1];
         });
       }
       if( href.test(/^https:\/\/(wordpress|jetpack|woocommerce)\.com/) ) {
-        args.push('aff=4310');
+        args.aff = 4310;
       }
-      $.each([
-        'utm_source=capitalp',
-        'utm_campaign=SponsorUs',
-        'utm_medium=voluntary_link'
-      ], function(j, u){
-        args.push(u);
-      });
-      parts[1] = args.join('&');
-      $(this).attr('href', parts.join('?'));
+      var params = [];
+      for(var prop in args){
+        if(args.hasOwnProperty(prop)){
+          params.push(prop + '=' + args[prop]);
+        }
+      }
+      parts[1] = params.join('&');
+      console.log(parts);
+      $(link).attr('href', parts.join('?'));
     }
   });
 
